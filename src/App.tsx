6b9e7e289259d99727a5533258ea1c2d4bcab41f
Jay Patel <jay64441860@gmail.com>
2024-01-16 12:27:50 +0530

    added animations

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import React, {
 } from "react";
 import "./App.css";
 import { Users } from "./constants";
+import { AnimatePresence, motion } from "framer-motion";
 
 export interface Chip {
     id: number;
@@ -26,7 +27,7 @@ const App: React.FC = () => {
     const inputRef = useRef<HTMLInputElement>(null);
     const contRef = useRef<HTMLDivElement>(null);
     
-    const users = useMemo(() => [...Users], [Users]);    
+    const users = useMemo(() => [...Users], []);    
 
     useEffect(() => {
         setFilteredItems(
@@ -38,9 +39,9 @@ const App: React.FC = () => {
         const handleClickOutside = (event: MouseEvent) => {
             if (contRef.current && !contRef.current.contains(event.target as Node)) {
                 setShowFilters(false);
+                setHighlighted(null);
             }
         };
-        setHighlighted(null);
 
         document.addEventListener("mousedown", handleClickOutside);
 
@@ -53,21 +54,10 @@ const App: React.FC = () => {
         const value = event.target.value;
         setInputValue(value);
         setHighlighted(null);
-
-        if (value === "" && chips.length > 0) {
-            highlightLastChip();
-        } else {
-            setFilteredItems(
-                users.filter((item) => !chips.find((chip) => chip.name.toLowerCase() === item.name.toLowerCase()) && item.name.toLowerCase().includes(value.toLowerCase()))
-            );
-        }
-    };
-
-    const highlightLastChip = () => {
-        const lastChip = document.querySelector(".chip:last-child");
-        if (lastChip) {
-            lastChip.classList.add("bg-red-200");
-        }
+        
+        setFilteredItems(
+            users.filter((item) => !chips.find((chip) => chip.name.toLowerCase() === item.name.toLowerCase()) && item.name.toLowerCase().includes(value.toLowerCase()))
+        );
     };
 
     const handleInputKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
@@ -105,18 +95,33 @@ const App: React.FC = () => {
     };
 
     return (
-        <div className="flex flex-col gap-8">
-            <div className="flex flex-wrap gap-8">
+        <div className="flex flex-col gap-8 font-sans p-5">
+            <div className="flex flex-wrap gap-2 items-center border-b-2 border-blue-600 pb-1 ">
+                <AnimatePresence>
                 {chips.map(chip => (
-                    <div
-                        key={chip.id}
-                        className={`text-white rounded px-2 py-1 flex items-center cursor-pointer ${highlighted?.id === chip.id ? "bg-red-500" : "bg-green-500"}`}
-                        onClick={() => removeChip(chip.id)}
-                    >
-                        {chip.name}
-                        <span className="ml-2 cursor-pointer">×</span>
-                    </div>
+                        <motion.div
+                            key={chip.id}
+                            initial={{ width: 0 }}
+                            animate={{ width: "auto" }}
+                            exit={{ width: 0 }}
+                            transition={{ ease: "easeOut", duration: 0.2 }}
+                            className={`rounded-full flex gap-1 items-center h-8 ${highlighted?.id === chip.id ? "bg-[#bbb]" : "bg-[#dfdfdf]"}`}
+                            
+                        >
+                            <img src={chip.avatar} alt={chip.name} className="w-8 h-8 rounded-full object-cover" />
+                            <div className="text-black/70 truncate">
+                                {chip.name}
+                            </div>
+
+                            <span 
+                                className="cursor-pointer text-black scale-150 mr-3 ml-1 -translate-y-0.5 hover:text-black/50 hover:scale-125 transition-all duration-200"
+                                onClick={() => removeChip(chip.id)}
+                                >
+                                ×
+                            </span>
+                        </motion.div>
                 ))}
+                </AnimatePresence>
                 <div className="relative" ref={contRef}>
                     <input
                         ref={inputRef}
@@ -126,34 +131,33 @@ const App: React.FC = () => {
                         onKeyDown={handleInputKeyDown}
                         onFocus={() => setShowFilters(true)}
                         placeholder="Type here..."
-                        className="p-2"
+                        className="p-2 border-none outline-none"
                     />
 
-                    {showFilters && (
-                        <div className="absolute top-full left-0 bg-gray-200 border border-gray-300 max-h-32 overflow-y-auto z-10">
-                            {filteredItems.map((item) => {
-                                const startIndex = item.name.toLowerCase().indexOf(inputValue.toLowerCase());
-                                const endIndex = startIndex + inputValue.length;
+                
+                    <div className={`absolute top-[calc(100%+6px)] left-0 bg-gray-200  overflow-y-auto  [&::-webkit-scrollbar]:hidden z-10 ${showFilters ? "max-h-56" : "max-h-0"} transition-all duration-200`}>
+                        {filteredItems.map((item) => {
+                            const startIndex = item.name.toLowerCase().indexOf(inputValue.toLowerCase());
+                            const endIndex = startIndex + inputValue.length;
 
-                                return (
-                                    <div
-                                        key={item.id}
-                                        className="p-2 cursor-pointer"
-                                        onClick={() => addChip(item)}
-                                    >
-                                        {item.name.split("").map((letter, index) => (
-                                                <span
-                                                    key={index}
-                                                    className={`${index >= startIndex && index < endIndex && "font-extrabold"}`}
-                                                >
-                                                    {letter}
-                                                </span>
-                                            ))}
-                                    </div>
-                                );
-                            })}
-                        </div>
-                    )}
+                            return (
+                                <div
+                                    key={item.id}
+                                    className="p-2 cursor-pointer hover:bg-[#ccc]/40"
+                                    onClick={() => addChip(item)}
+                                >
+                                    {item.name.split("").map((letter, index) => (
+                                            <span
+                                                key={index}
+                                                className={`${index >= startIndex && index < endIndex && "font-extrabold"}`}
+                                            >
+                                                {letter}
+                                            </span>
+                                        ))}
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
